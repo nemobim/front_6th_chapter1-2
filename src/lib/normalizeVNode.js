f3e2d611f47a95cc69fe7c979b6fa0649c86d134
nemo-nemo-bim <drakequation@naver.com>
2025-07-16 01:58:45 +0900

    feat: normalizeVNode 구현

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,38 @@
 export function normalizeVNode(vNode) {
+  console.log("시작~~~~~~~~~~~~~~~~~~~");
+  if (vNode === null || vNode === undefined || typeof vNode === "boolean") {
+    return "";
+  }
+
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return vNode.toString();
+  }
+
+  if (typeof vNode.type === "function") {
+    const componentProps = {
+      ...(vNode.props || {}),
+      children: vNode.children,
+    };
+
+    const renderedComponent = vNode.type(componentProps);
+    return normalizeVNode(renderedComponent);
+  }
+
+  if (typeof vNode === "object" && vNode?.type) {
+    console.log("vNode object", vNode);
+    const normalizedChildren = (vNode.children || []).map(normalizeVNode).filter((child) => child !== "");
+
+    console.log("vNode object 반환값", {
+      ...vNode,
+      children: normalizedChildren,
+    });
+    return {
+      ...vNode,
+      children: normalizedChildren,
+    };
+  }
+
+  console.log("vNode", vNode);
+
   return vNode;
 }
